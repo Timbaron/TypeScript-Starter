243f3fffd5b0b00d016ec22e585e2708398d1147
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-28 10:30:03 +0100

    Dynamic (any) Types

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,20 @@
-// EXPLICIT TYPES
-var character;
 var age;
-var isLoggedIn;
-// age = 'Timothy'; // Will fail because age is explicitly typed as a number 
-age = 40; // Will pass because age is explicitly typed as a number
-// isLoggedIn = 25; // Will fail because isLoggedIn is explicitly typed as a boolean
-isLoggedIn = true; // Will pass because isLoggedIn is explicitly typed as a boolean
-// ARRAYS
-var myArray = [];
-// myArray = [12, 25, 36]; // Will fail because myArray is explicitly typed as a string array
-myArray = ['Timothy', 'John', 'Jane'];
-// UNION TYPES
+age = 25;
+// same as let age: any = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'Max' };
+console.log(age);
 var mixed = [];
-mixed.push('Timothy');
-mixed.push(25);
-// mixed.push(false); // Will fail because mixed is explicitly typed as a string or number array
+mixed.push(5);
+mixed.push('Samuel');
+mixed.push(false);
 console.log(mixed);
-var uid; // Can be a string or a number
-uid = '123';
-uid = 123;
-// uid = false; // Will fail because uid is explicitly typed as a string or number
-// OBJECTS
+// objects
 var Akiode;
-Akiode = { name: 'Timothy', age: 40 };
-// Akiode = 'Timothy'; // Will fail because Akiode is explicitly typed as an object
-var Akiode2;
-//  Akiode2 = [12, 25, 36]; // Will fail because Akiode2 is explicitly typed as an object with name, age, and isLoggedIn properties
-Akiode2 = { name: 'Timothy', age: 40, isLoggedIn: true }; // Will pass because Akiode2 is explicitly typed as an object with the properties name, age, and isLoggedIn
+Akiode = { name: 'Akiode', age: 25 };
+console.log(Akiode);
+Akiode = { name: 'Akiode', age: '25' };
+console.log(Akiode);
--- sandbox.ts
+++ sandbox.ts
@@ -1,50 +1,39 @@
-// EXPLICIT TYPES
+let age: any;
 
-let character:string;
-let age:number;
-let isLoggedIn:boolean;
+age = 25;
 
-// age = 'Timothy'; // Will fail because age is explicitly typed as a number 
-age = 40; // Will pass because age is explicitly typed as a number
-// isLoggedIn = 25; // Will fail because isLoggedIn is explicitly typed as a boolean
-isLoggedIn = true; // Will pass because isLoggedIn is explicitly typed as a boolean
+// same as let age: any = 25;
 
+age = true;
 
-// ARRAYS
-let myArray:string[] = [];
+console.log(age);
 
-// myArray = [12, 25, 36]; // Will fail because myArray is explicitly typed as a string array
-myArray = ['Timothy', 'John', 'Jane'];
+age = 'hello';
 
-// UNION TYPES
+console.log(age);
 
-let mixed: (string|number)[] = [];
+age = { name: 'Max' };
 
-mixed.push('Timothy');
-mixed.push(25);
-// mixed.push(false); // Will fail because mixed is explicitly typed as a string or number array
+console.log(age);
+
+let mixed: any[] = [];
+
+mixed.push(5);
+
+mixed.push('Samuel');
+
+mixed.push(false);
+
 console.log(mixed);
 
-let uid: string|number; // Can be a string or a number
+// objects
 
-uid = '123';
-uid = 123;
-// uid = false; // Will fail because uid is explicitly typed as a string or number
+let Akiode: { name: any; age: any };
 
+Akiode = { name: 'Akiode', age: 25 };
 
-// OBJECTS
+console.log(Akiode);
 
-let Akiode: object;
+Akiode = { name: 'Akiode', age: '25' };
 
-Akiode = {name: 'Timothy', age: 40};
-
-// Akiode = 'Timothy'; // Will fail because Akiode is explicitly typed as an object
-
-let Akiode2: {
-    name: string,
-    age: number,
-    isLoggedIn: boolean
-}
-
-//  Akiode2 = [12, 25, 36]; // Will fail because Akiode2 is explicitly typed as an object with name, age, and isLoggedIn properties
-Akiode2 = {name: 'Timothy', age: 40, isLoggedIn: true}; // Will pass because Akiode2 is explicitly typed as an object with the properties name, age, and isLoggedIn
+console.log(Akiode);
