2d0efae54eebc425dfcfb12de5a013a8154217cb
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-28 11:10:45 +0100

    Function Signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,10 +1,32 @@
-type StringOrNum = string|number;
-type objWithName = {name: string, uid: StringOrNum};
+// let greet:Function;
 
-const logDetails = (uid:StringOrNum, item:string) => {
-    console.log(`${item} has a uid of ${uid}`);
+// Example 1
+
+let greet:(a:string,b:string) => void;
+
+greet = (name:string, greeting:string) => {
+    console.log(`${name} says ${greeting}`)
 }
 
-const greet = (user:objWithName) => {
-    console.log(`${user.name} says hello`);
+// Example 2
+
+let calc: (a:number, b:number, c:string) => number;
+
+calc = (numOne:number, numTwo:number, action:string) => {
+    if(action === 'add'){
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+}
+
+// Example 3
+
+let logDetails: (obj:{name:string, age:number}) => void;
+
+type person = {name:string, age:number};
+
+logDetails = (akiode: person) => {
+    console.log(`${akiode.name} is ${akiode.age} years old`)
 }
