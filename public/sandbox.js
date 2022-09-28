7ea7a933edf20373a3e55546b0eabb35ce59b70a
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-28 11:01:26 +0100

    Type Aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,21 +1,7 @@
 "use strict";
-// const greet = () => {
-//     console.log('Hello World');
-// }
-let greet;
-// greet = 'Hello World'; // Error: Cannot assign to 'greet' because it is a constant or a read-only property.
-greet = () => {
-    console.log('Hello World');
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-const add = (a, b, c = 10) => {
-    console.log(a + b);
-    console.log(c);
+const greet = (user) => {
+    console.log(`${user.name} says hello`);
 };
-// returns void when no return statement is present
-add(5, 10, '20');
-const minus = (a, b) => {
-    return a + b;
-};
-// returns number when return statement is present
-let result = minus(10, 7);
-// result = 'String'; // Error: Type 'string' is not assignable to type 'number'.
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,28 +1,10 @@
-// const greet = () => {
-//     console.log('Hello World');
-// }
+type StringOrNum = string|number;
+type objWithName = {name: string, uid: StringOrNum};
 
-let greet: Function;
-
-// greet = 'Hello World'; // Error: Cannot assign to 'greet' because it is a constant or a read-only property.
-
-greet = () => {
-    console.log('Hello World');
+const logDetails = (uid:StringOrNum, item:string) => {
+    console.log(`${item} has a uid of ${uid}`);
 }
 
-const add = (a:number, b:number, c:number|string = 10):void => {
-    console.log(a+b)
-    console.log(c)
-} 
-// returns void when no return statement is present
-
-add(5, 10,'20');
-
-const minus = (a:number, b:number):number => {
-    return a+b;
+const greet = (user:objWithName) => {
+    console.log(`${user.name} says hello`);
 }
-// returns number when return statement is present
-
-let result = minus(10, 7);
-
-// result = 'String'; // Error: Type 'string' is not assignable to type 'number'.
