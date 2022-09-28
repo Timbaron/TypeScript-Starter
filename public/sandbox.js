e246dafa539540d80fcb94be55931e62478da8eb
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-28 10:55:59 +0100

    Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('Text 3');
+// const greet = () => {
+//     console.log('Hello World');
+// }
+let greet;
+// greet = 'Hello World'; // Error: Cannot assign to 'greet' because it is a constant or a read-only property.
+greet = () => {
+    console.log('Hello World');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+// returns void when no return statement is present
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a + b;
+};
+// returns number when return statement is present
+let result = minus(10, 7);
+// result = 'String'; // Error: Type 'string' is not assignable to type 'number'.
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,28 @@
-console.log('Text 3')
+// const greet = () => {
+//     console.log('Hello World');
+// }
+
+let greet: Function;
+
+// greet = 'Hello World'; // Error: Cannot assign to 'greet' because it is a constant or a read-only property.
+
+greet = () => {
+    console.log('Hello World');
+}
+
+const add = (a:number, b:number, c:number|string = 10):void => {
+    console.log(a+b)
+    console.log(c)
+} 
+// returns void when no return statement is present
+
+add(5, 10,'20');
+
+const minus = (a:number, b:number):number => {
+    return a+b;
+}
+// returns number when return statement is present
+
+let result = minus(10, 7);
+
+// result = 'String'; // Error: Type 'string' is not assignable to type 'number'.
