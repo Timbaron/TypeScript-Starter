67a401ca7cc6bbb6bc20df0afb3179d44115bf00
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-29 13:03:03 +0100

    Rendering an HTML Template

--- public/app.js
+++ public/app.js
@@ -1,39 +1,8 @@
-// Interface
-// interface IsPerson {
-//     name: string;
-//     age: number;
-//     speak(a:string): void
-//     spend(a:number) :number;
-// }
-// const me: IsPerson = {
-//     name: 'Timothy',
-//     age: 30,
-//     speak(text:string): void {
-//         console.log(text);
-//     },
-//     spend(amount:number): number {
-//         console.log('I spent', amount);
-//         return amount;
-//     },
-//skills: [] // error because skills is not defined in the interface
-// } 
-// const greetPerson = (person: IsPerson)=> {
-//     console.log('Hello', person.name);
-// }
-// greetPerson(name: 'Timothy'); // error because name is not defined in the interface
-// greetPerson(me); // works
-// let someone: IsPerson; 
-// console.log(me)
 import { Invoice } from './classess/invoice.js';
+import { ListTemplate } from './classess/ListTemplate.js';
 import { Payment } from './classess/Payment.js';
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('Samuel', 'Blood Donation', 700)
-// docTwo = new Payment('Deji', 'Service', 900);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs)
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 const InvOne = new Invoice('Timothy', 'work on the EBS website', 250);
 const InvTwo = new Invoice('Bishop', 'work on the Jollof website', 650);
 // console.log(InvOne, InvTwo);
@@ -44,13 +13,11 @@ invoice.push(InvTwo);
 invoice.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
 });
-//InvOne.client = 'Jesus'; // Error because client is readonly
 InvTwo.amount = 900;
 console.log(InvOne, InvTwo);
 console.log(invoice);
 // const form = document.querySelector('form');
 const form = document.querySelector('.new-item-form');
-// console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -65,5 +32,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,11 @@
-// Interface
-
-// interface IsPerson {
-//     name: string;
-//     age: number;
-//     speak(a:string): void
-//     spend(a:number) :number;
-// }
-
-// const me: IsPerson = {
-//     name: 'Timothy',
-//     age: 30,
-//     speak(text:string): void {
-//         console.log(text);
-//     },
-//     spend(amount:number): number {
-//         console.log('I spent', amount);
-//         return amount;
-//     },
-    //skills: [] // error because skills is not defined in the interface
-// } 
-
-// const greetPerson = (person: IsPerson)=> {
-//     console.log('Hello', person.name);
-// }
-
-// greetPerson(name: 'Timothy'); // error because name is not defined in the interface
-// greetPerson(me); // works
-
-// let someone: IsPerson; 
-// console.log(me)
 import {Invoice} from './classess/invoice.js'
+import { ListTemplate } from './classess/ListTemplate.js';
 import {Payment} from './classess/Payment.js'
 import {HasFormatter} from './Interfaces/HasFormatter.js'
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
 
-// docOne = new Invoice('Samuel', 'Blood Donation', 700)
-// docTwo = new Payment('Deji', 'Service', 900);
-
-// let docs: HasFormatter[] = [];
-
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs)
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 const InvOne = new Invoice('Timothy', 'work on the EBS website', 250);
 const InvTwo = new Invoice('Bishop', 'work on the Jollof website', 650);
@@ -58,7 +19,6 @@ invoice.push(InvTwo);
 invoice.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
 })
-//InvOne.client = 'Jesus'; // Error because client is readonly
 InvTwo.amount = 900;
 
 console.log(InvOne, InvTwo);
@@ -66,7 +26,6 @@ console.log(invoice);
 // const form = document.querySelector('form');
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -76,7 +35,7 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-
+ 
     let doc: HasFormatter;
     if(type.value === 'invoice') {
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
@@ -84,5 +43,5 @@ form.addEventListener('submit', (e: Event) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
     }
-    console.log(doc)
+    list.render(doc, type.value, 'end');
 })
