7f77a189da66a5582a31a717277bdcefbb0b3af0
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-29 04:59:17 +0100

    Classess

--- public/app.js
+++ public/app.js
@@ -1,9 +1,26 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// // if(anchor){
-// //     console.log(anchor.href);
-// // }
-// console.log(anchor.href);
+// Classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owns $${this.amount} for ${this.details}`;
+    }
+}
+const InvOne = new Invoice('Timothy', 'work on the EBS website', 250);
+const InvTwo = new Invoice('Bishop', 'work on the Jollof website', 650);
+// console.log(InvOne, InvTwo);
+let invoice = []; // array of invoices, Only invoices can be added to this array
+// invoice.push('hello'); // error because 'hello' is not an instance of invoice object
+invoice.push(InvOne);
+invoice.push(InvTwo);
+InvOne.client = 'Jesus';
+InvTwo.amount = 900;
+console.log(InvOne, InvTwo);
+console.log(invoice);
 // const form = document.querySelector('form');
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
